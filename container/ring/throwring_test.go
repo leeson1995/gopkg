eb57c1bff4e93a79e18a153ca4027a8029ef20af
leeson <leeson@anchytec.com>
2020-07-30 17:40:33 +0800

    throw benchtest

--- container/ring/throwring_test.go
+++ container/ring/throwring_test.go
@@ -6,56 +6,83 @@ import (
 	"time"
 )
 
-func TestGetSet(t *testing.T) {
-	q := NewThrowRing(5)
+func BenchmarkGetSet(b *testing.B) {
+	type TestStruct struct {
+		t int
+	}
+
+	q := NewThrowRing(100)
+	b.Run("Get", func(b *testing.B) {
+		go func() {
+
+			for {
+				g, ok := q.Get().(TestStruct)
+				if ok {
+					b.Log("get ", g)
+				}
+			}
+
+		}()
+	})
+
+	var count int
+
+	b.Run("set ", func(b *testing.B) {
+		for count < 1000 {
+			q.Add(TestStruct{count})
+			count++
+		}
+	})
+
+}
+
+func TestGetLag(t *testing.T) {
+	type TickRecord struct {
+		t int
+	}
+	q := NewThrowRing(10)
 	var count int
 	go func() {
-
 		for {
 			g, ok := q.Get().(TickRecord)
 			if ok {
 				fmt.Println("get ", g)
-			} else {
-				fmt.Println("nil nil nil")
 			}
 
-			time.Sleep(time.Millisecond)
+			time.Sleep(time.Millisecond * 40)
 		}
 
 	}()
 
-	for {
+	for count < 100 {
 		q.Add(TickRecord{count})
-		(q.String())
 		count++
-		time.Sleep(time.Millisecond * 2)
+		time.Sleep(time.Millisecond * 20)
 	}
-}
 
-type TickRecord struct {
-	t int
 }
-
-func BenchmarkGetSetBench(t *testing.B) {
-	q := NewThrowRing(5)
+func TestSetLag(t *testing.T) {
+	type TickRecord struct {
+		t int
+	}
+	q := NewThrowRing(10)
 	var count int
 	go func() {
-
 		for {
 			g, ok := q.Get().(TickRecord)
 			if ok {
 				fmt.Println("get ", g)
 			}
 
-			time.Sleep(time.Millisecond * 2)
+			time.Sleep(time.Millisecond * 20)
 		}
 
 	}()
 
-	for count < 1000 {
+	for count < 100 {
 		q.Add(TickRecord{count})
-		(q.String())
 		count++
-		time.Sleep(time.Millisecond * 3)
+		time.Sleep(time.Millisecond * 40)
 	}
+
 }
