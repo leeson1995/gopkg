a14d924d16e6474a78c47cb50f5277a6b7ad71ff
leeson <leeson@anchytec.com>
2020-08-07 10:34:04 +0800

    throw benchtest

--- container/ring/throwring.go
+++ container/ring/throwring.go
@@ -18,14 +18,42 @@ type ThrowRing struct {
 	//ring 长度
 	size int
 	mu sync.Mutex
+
+	opt options
 }
 
-func NewThrowRing(size int) *ThrowRing {
-	return &ThrowRing{
+type Opt interface {
+	apply(*options)
+}
+
+// **尝试**获取即将被覆盖数据,buffer越大，丢失几率越低
+func WithTakeThrow(ThrowBufferSize int) Opt {
+	return newOptFuncImpl(func(o *options) {
+		o.IsTakeThrow = true
+		o.ThrowBufferSize = ThrowBufferSize
+		o.ThrowC = make(chan interface{}, ThrowBufferSize)
+	})
+}
+
+var DefaultOpt = options{}
+
+//size mast be 2^n
+func NewThrowRing(size int, o ...Opt) *ThrowRing {
+	if size <= 0 && (size&(size-1)) != 0 {
+		panic("param size mast be 2^n and can not be zero")
+	}
+
+	t := &ThrowRing{
 		buf:  make([]interface{}, size),
 		size: size,
 		mu:   sync.Mutex{},
+		opt:  DefaultOpt,
 	}
+
+	for _, v := range o {
+		v.apply(&t.opt)
+	}
+	return t
 }
 
 func (q *ThrowRing) Add(elem interface{}) {
@@ -33,9 +61,10 @@ func (q *ThrowRing) Add(elem interface{}) {
 	q.mu.Lock()
 	q.buf[q.head] = elem
 	//next head
-	q.head = (q.head + 1) % (q.size)
+	q.head = (q.head + 1) & (q.size - 1)
 	if q.head == q.tail {
-		q.tail = (q.tail + 1) % (q.size)
+		q.beforeThrow()
+		q.tail = (q.tail + 1) & (q.size - 1)
 	}
 	q.mu.Unlock()
 }
@@ -45,7 +74,7 @@ func (q *ThrowRing) Get() interface{} {
 
 	ret := q.buf[q.tail]
 	q.buf[q.tail] = nil
-	q.tail = (q.tail + 1) % (q.size)
+	q.tail = (q.tail + 1) & (q.size - 1)
 
 	q.mu.Unlock()
 
@@ -55,4 +84,39 @@ func (q *ThrowRing) Get() interface{} {
 
 func (q *ThrowRing) String() {
 	fmt.Printf("ThrowRing: %+v \n ", q)
+}
+
+func (q *ThrowRing) GetThrowC() <-chan interface{} {
+	if q.opt.IsTakeThrow {
+		return q.opt.ThrowC
+	}
+	return nil
+}
+
+func (q *ThrowRing) beforeThrow() {
+	if q.opt.IsTakeThrow &&
+		len(q.opt.ThrowC) < q.opt.ThrowBufferSize {
+		q.opt.ThrowC <- q.buf[q.tail]
+	}
+
+}
+
+type options struct {
+	IsTakeThrow     bool
+	ThrowBufferSize int
+	ThrowC          chan interface{}
+}
+
+type optFunc func(o *options)
+
+type optFuncImpl struct {
+	f optFunc
+}
+
+func (odo *optFuncImpl) apply(o *options) {
+	odo.f(o)
+}
+
+func newOptFuncImpl(f optFunc) *optFuncImpl {
+	return &optFuncImpl{f: f}
 }
--- container/ring/throwring_test.go
+++ container/ring/throwring_test.go
@@ -1,3 +1,7 @@
+// Copyright 2016 ~ 2019 Leeson(https://github.com/Leeson1995).
+// All rights reserved.  Use of this source code is
+// governed by Apache License 2.0.
+
 package ring
 
 import (
@@ -83,6 +87,42 @@ func TestSetLag(t *testing.T) {
 		q.Add(TickRecord{count})
 		count++
 		time.Sleep(time.Millisecond * 40)
+	}
+
+}
+func TestWithTakeThrow(t *testing.T) {
+	type TickRecord struct {
+		t int
+	}
+	q := NewThrowRing(100, WithTakeThrow(100))
+	var count int
+	go func() {
+		c := q.GetThrowC()
+		if c != nil {
+
+			for {
+				d := <-c
+				fmt.Println("thrwo: ", d)
+			}
+		}
+
+	}()
+	go func() {
+		for {
+			g, ok := q.Get().(TickRecord)
+			if ok {
+				fmt.Println("get ", g)
+			}
+
+			time.Sleep(time.Millisecond * 40)
+		}
+
+	}()
+
+	for count < 100 {
+		q.Add(TickRecord{count})
+		count++
+		time.Sleep(time.Millisecond * 20)
 	}
 
 }
