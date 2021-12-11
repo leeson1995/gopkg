7b19848f9d779d30eb419108fbfdf29e0a5d385d
leeson1995 <15711363789@163.com>
2021-12-12 02:09:31 +0800

    add kafka

---
+++ utils/md5.go
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * // Author: leeson9616@gmail.com
+ * // Update：
+ ******************************************************************************/
+
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+func Md5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	cipher := h.Sum(nil)
+	return hex.EncodeToString(cipher)
+}
--- utils/str.go
+++ utils/str.go
@@ -6,7 +6,10 @@
 package utils
 
 import (
+	"encoding/json"
+	"golang.org/x/exp/rand"
 	"reflect"
+	"strconv"
 	"unsafe"
 )
 
@@ -26,6 +29,59 @@ func Bytes2String(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
+func IntToString(i int) string {
+	return strconv.FormatInt(int64(i), 10)
+}
+
+func StringToInt(i string) int {
+	j, _ := strconv.Atoi(i)
+	return j
+}
+func StringToInt64(i string) int64 {
+	j, _ := strconv.ParseInt(i, 10, 64)
+	return j
+}
+
+//judge a string whether in the  string list
+func IsContain(target string, List []string) bool {
+	for _, element := range List {
+
+		if target == element {
+			return true
+		}
+	}
+	return false
+
+}
+
+func InterfaceArrayToStringArray(data []interface{}) (i []string) {
+	for _, param := range data {
+		i = append(i, param.(string))
+	}
+	return i
+}
+func StructToJsonString(param interface{}) string {
+	dataType, _ := json.Marshal(param)
+	dataString := string(dataType)
+	return dataString
+}
+
+//The incoming parameter must be a pointer
+func JsonStringToStruct(s string, args interface{}) error {
+	err := json.Unmarshal([]byte(s), args)
+	return err
+}
+
+func GetMsgID(sendID string) string {
+	t := int64ToString(GetCurrentTimestampByNano())
+	return Md5(t + sendID + int64ToString(rand.Int63n(GetCurrentTimestampByNano())))
+}
+func int64ToString(i int64) string {
+	return strconv.FormatInt(i, 10)
+}
+func Int64ToString(i int64) string {
+	return strconv.FormatInt(i, 10)
+}
 
 //FYR gin
 //func StringToBytes(s string) []byte {
